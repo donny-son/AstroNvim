007be2412603dd6224d43c1362262dc290eb2a8e
donny-son <dongook.son@gmail.com>
2023-03-11 14:32:45 +0900

    Change notify popup setting

--- lua/plugins/configs/notify.lua
+++ lua/plugins/configs/notify.lua
@@ -1,5 +1,22 @@
 return function(_, opts)
   local notify = require "notify"
-  notify.setup(opts)
+  notify.setup {
+    background_colour = "Normal",
+    fps = 60,
+    icons = {
+      DEBUG = "",
+      ERROR = "",
+      INFO = "",
+      TRACE = "✎",
+      WARN = "",
+    },
+    level = 2,
+    minimum_width = 50,
+    render = "default",
+    stages = "fade_in_slide_out",
+    timeout = 2000,
+    top_down = false,
+  }
+
   vim.notify = notify
 end
