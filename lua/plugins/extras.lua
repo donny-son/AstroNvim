b227b49e42a012353197eeb98da2d66f3a0542b3
Dongook Son <60206749+donny-son@users.noreply.github.com>
2023-03-12 02:31:40 +0900

    Add extra packages from previous config

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    event = "User AstroFile",
+  },
+  {
+    "declancm/cinnamon.nvim",
+    event = "User AstroFile",
+  },
+  {
+    "mechatroner/rainbow_csv",
+    event = "User AstroFile",
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && yarn install",
+    event = "User AstroFile",
+  },
+}
